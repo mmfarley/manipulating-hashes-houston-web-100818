2b1dbf6e47b06718215da1bba3b1363c2b522fdd
Megan Farley <megfarley4@gmail.com>
2018-10-05 01:25:31 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
   #your code here
   contacts["Freddy Mercury"].each do |key, value|
     if key == favorite_icecream_flavors
-      value.delete_if 
+      value.delete_if {|item| item == "strawberry"}
       end
     end
 
